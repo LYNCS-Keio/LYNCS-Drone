6da083faee40dc12cb0a1e4f0e48a05a8c625c30
Naoki Yano <puni87gou@gmail.com>
2019-07-23 17:23:08 +0900

    I2C libs (#5)
    * Initial commit, beta state
    
    * Reestrutured directory and added example/test
    
    * adjusted log level to verbose to debug lib
    
    * redone directory structure
    
    * Created menu options for I2Cbus in menuconfig
    
    * add initial readme.md
    
    * update readme.md
    
    * add port number to log
    
    * Add timeout to testConnection method
    
    * delete sdkconfig
    
    * modify log defines and add destructor
    
    * Modified class name to I2Cbus_t and header filename to .hpp
    
    * Refactor, multiple modifications and added license file
    
    * Update README.me
    
    * Update README.md
    
    * Small various changes
    
    * Update README.md
    
    * Changed class to I2C, objects to i2c0 and i2c1
    
    * Aligned log ouput error phrases and decrease scanner timeout
    
    * Added getter method for default objects
    
    * Update README.md
    
    * Updated ACK parameter in i2c driver calls. Nested I2C class in namespace
    
    * Update README.md
    
    * Add initial support to CMake build system
    
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>
    
    * Improve CMake build support
    
    * renamed main.cpp
    
    * C++用に変更
    
    * I2Cbusを使おう
    
    * update

---
+++ ESP32/src/main.cpp
@@ -0,0 +1,17 @@
+// =========================================================================
+// This code is placed under the MIT License
+// Copyright 2019 Laboratory of sYNsetic Cosmic Science. All rights reserved.
+// For the license information refer to LICENSE file in root directory.
+// =========================================================================
+
+/**
+ * @file main.cpp
+ * Main function.
+ */
+#include <stdio.h>
+#include <I2Cbus.hpp>
+
+
+extern "C" void app_main()
+{
+}
