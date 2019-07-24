6eea5a040d3992446080f132d067e34094441bc8
NaokiYano <puni87gou@gmail.com>
2019-07-24 21:30:14 +0900

    積分してなかった

--- ESP32/lib/PIDController/src/PIDController.cpp
+++ ESP32/lib/PIDController/src/PIDController.cpp
@@ -18,7 +18,7 @@ PIDController::~PIDController()
 void PIDController::InputPID(double data, double goal, double dt)
 {
 	double error = goal - data;
-	integral_ = error * dt;
+	integral_ += error * dt;
 	manipulative_variable_ = kProportionGain_ * error + kIntegralGain_ * integral_ + kDifferentialGain_ * (error - prev_error_) / dt;
 	prev_error_ = error;
 }
