a14de6286171156fb8c675a9d0be2fc9b9c55d49
NaokiYano <puni87gou@gmail.com>
2019-08-16 07:37:24 +0900

    [ADD] Mode

--- ESP32/lib/DPSdriver/util/dps_const.hpp
+++ ESP32/lib/DPSdriver/util/dps_const.hpp
@@ -2,11 +2,23 @@
 
 namespace dps
 {
-    enum dps_err_t{
-        DPS__SUCCEEDED = 0,
-        DPS__FAIL_UNKNOWN = -1,
-        DPS__FAIL_INIT_FAILED = -2,
-        DPS__FAIL_TOOBUSY = -3,
-        DPS__FAIL_UNFINISHED = -4
-    };
+enum dps_err_t
+{
+    DPS__SUCCEEDED = 0,
+    DPS__FAIL_UNKNOWN = -1,
+    DPS__FAIL_INIT_FAILED = -2,
+    DPS__FAIL_TOOBUSY = -3,
+    DPS__FAIL_UNFINISHED = -4
+};
+
+enum Mode
+{
+    IDLE = 0x00,
+    CMD_PRS = 0x01,
+    CMD_TEMP = 0x02,
+    CMD_BOTH = 0x03, // only for DPS422
+    CONT_PRS = 0x05,
+    CONT_TMP = 0x06,
+    CONT_BOTH = 0x07
+};
 } // namespace dps
