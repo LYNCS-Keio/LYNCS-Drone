272a45dc53bf1396553c9509e496a073a88adb7a
NaokiYano <puni87gou@gmail.com>
2019-08-16 07:29:57 +0900

    [ADD]dps_err_t

---
+++ ESP32/lib/DPSdriver/util/dps_const.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace dps
+{
+    enum dps_err_t{
+        DPS__SUCCEEDED = 0,
+        DPS__FAIL_UNKNOWN = -1,
+        DPS__FAIL_INIT_FAILED = -2,
+        DPS__FAIL_TOOBUSY = -3,
+        DPS__FAIL_UNFINISHED = -4
+    }
+} // namespace dps
