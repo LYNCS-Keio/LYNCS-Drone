ce738d6355fbc87e786d579a48f285826e098e1f
Naoki Yano <puni87gou@gmail.com>
2019-09-18 22:55:05 +0900

    [ADD]DPS__FAIL_COMMUNICATION

--- ESP32/lib/DPSdriver/util/dps_const.hpp
+++ ESP32/lib/DPSdriver/util/dps_const.hpp
@@ -42,7 +42,8 @@ enum dps_err_t
     DPS__FAIL_INIT_FAILED = -2,
     DPS__FAIL_TOOBUSY = -3,
     DPS__FAIL_UNFINISHED = -4,
-    DPS__FAIL_CANNOT_WRITE_REG = -5
+    DPS__FAIL_CANNOT_WRITE_REG = -5,
+    DPS__FAIL_COMMUNICATION = -6
 };
 
 enum Mode
