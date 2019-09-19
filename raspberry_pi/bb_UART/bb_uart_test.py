fbcc7988227bd9ac5e772c9be724a2611af0504a
RyosukeSasaki <rsasaki@keio.jp>
2019-09-19 19:54:19 +0900

    format receiving data

--- raspberry_pi/bb_UART/bb_uart_test.py
+++ raspberry_pi/bb_UART/bb_uart_test.py
@@ -2,10 +2,17 @@ import pigpio
 
 pi = pigpio.pi()
 try:
+    pi.bb_serial_read_close(17)
     status = pi.bb_serial_read_open(17, 9600)
     while True:
-        count, data = pi.bb_serial_read(17)
-        print(data, end="")
+        count = 1
+        text = ""
+        while count:
+            count, data = pi.bb_serial_read(17)
+            if count:
+                text += data
+            time.sleep(0.01)
+        print(text)
 
 finally:
     pi.bb_serial_read_close(17)
