39bf9828c3f2a4ab7c5ac88f64c8a0628e4d01d4
RyosukeSasaki <rsasaki@keio.jp>
2019-09-19 19:26:11 +0900

    bit bang uart test

---
+++ raspberry_pi/bb_UART/bb_uart_test.py
@@ -0,0 +1,11 @@
+import pigpio
+
+pi = pigpio.pi()
+try:
+    status = pi.bb_serial_read_open(17, 9600)
+    while True:
+        count, data = pi.bb_serial_read(17)
+        print(data)
+
+finally:
+    pi.bb_serial_read_close(17)
