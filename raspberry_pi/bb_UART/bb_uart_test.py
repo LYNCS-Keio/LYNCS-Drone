dada4716f108672676c84f55f870326a0e921d6d
RyosukeSasaki <rsasaki@keio.jp>
2019-09-19 19:34:31 +0900

    mod

--- raspberry_pi/bb_UART/bb_uart_test.py
+++ raspberry_pi/bb_UART/bb_uart_test.py
@@ -5,7 +5,7 @@ try:
     status = pi.bb_serial_read_open(17, 9600)
     while True:
         count, data = pi.bb_serial_read(17)
-        print(data)
+        print(count)
 
 finally:
     pi.bb_serial_read_close(17)
