49e00b7b54d223ad4c0b26fdfd6a7a2f1678e5dc
Naoki Yano <puni87gou@gmail.com>
2019-09-27 20:06:17 +0900

    :+1:add some register map

--- ESP32/lib/MPUdriver/util/mpu_const.hpp
+++ ESP32/lib/MPUdriver/util/mpu_const.hpp
@@ -13,9 +13,23 @@ namespace mpu
     };
     enum Config_Registers_e
     {
-        MPU__WHO_AM_I = 0
+        MPU__WHO_AM_I = 0,
+        BYPASS_EN,
+        H_RESET,
+        SLEEP,
+        CYCLE,
+        GYRO_STANDBY,
+        PD_PTAT,
+        CLKSEL
     };
     const RegMask_t registers[] = {
-        {0x75, 0xFF, 0}
+        {0x75, 0xFF, 0},    //WHO_AM_I
+        {0x37, 0x02, 1},    //BYPASS_EN        
+        {0x6B, 0x80, 7},    //H_RESET
+        {0x6B, 0x40, 6},    //SLEEP
+        {0x6B, 0x20, 5},    //CYCLE
+        {0x6B, 0x10, 4},    //GYRO_STANDBY
+        {0x6B, 0x08, 3},    //PD_PTAT
+        {0x6B, 0x07, 0},    //CLKSEL
     };
 } // namespace mpu
