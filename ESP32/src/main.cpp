fafada6856ef51e88073b347f45cb6cce7a000f3
Naoki Yano <puni87gou@gmail.com>
2019-09-27 19:19:52 +0900

    :truck:renamed some variables

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -19,8 +19,14 @@
 #include "esp_err.h"
 #include "math.h"
 
-#define SPI_CLOCK   10000000  // 10 MHz
-#define SPI_MODE    3
+#define DPS_SPI_CLOCK   10000000  // 10 MHz
+#define DPS_SPI_MODE    3
+
+#define DPS_MISO_PIN    19
+#define DPS_MOSI_PIN    23
+#define DPS_SCLK_PIN    18
+#define DPS_CS_PIN      14
+
 
 #define MISO_PIN    19
 #define MOSI_PIN    23
@@ -32,9 +38,9 @@ extern "C" void app_main()
     SPI_t &mySPI = hspi;  // vspi and hspi are the default objects
 
     spi_bus_config_t config;
-    config.mosi_io_num = MOSI_PIN;
-    config.miso_io_num = MISO_PIN;
-    config.sclk_io_num = SCLK_PIN;
+    config.mosi_io_num = DPS_MOSI_PIN;
+    config.miso_io_num = DPS_MISO_PIN;
+    config.sclk_io_num = DPS_SCLK_PIN;
     config.quadwp_io_num = -1; // -1 not used
     config.quadhd_io_num = -1; // -1 not used
     config.max_transfer_sz = 4094;
@@ -43,7 +49,7 @@ extern "C" void app_main()
     spi_bus_initialize(HSPI_HOST, &config, 0); // 0 DMA not used
     
     dps310::DPS310 myDPS(&mySPI);
-    myDPS.dev_init(SPI_MODE,SPI_CLOCK,CS_PIN);
+    myDPS.dev_init(DPS_SPI_MODE,DPS_SPI_CLOCK,DPS_CS_PIN);
     myDPS.setTmpOversamplingRate(1);
     myDPS.setPrsOversamplingRate(1);
 
